d5d1eb3a792d687f9736c4cce307a248bd3c857d
Kegan <strawn.kegan@gmail.com>
2016-12-21 21:33:32 -0700

    Project Page Updated to have the new fields

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -3,7 +3,28 @@
 @section('content')
 <h1>{{ $project->title }}</h1>
 <h3>{{ $project->description }}</h3>
-<a href="/members/{{ $project->id }}">Members</a>
+
+@if($project->github_link) 
+	<h3><a href="{{$project->github_link}}">GitHub Link</a></h3>
+@endif
+
+@if($project->slack_link) 
+	<h3><a href="{{$project->slack_link}}">Slack Link</a></h3>
+@endif
+
+@if($project->website_link) 
+	<h3>{{ $project->website_link }}</h3>
+@endif
+
+@if($project->get_involved_pitch) 
+	<h3>{{ $project->get_involved_pitch }}</h3>
+@endif
+
+@if($project->project_admin_user_id) 
+	<h3><a href="/user/{{$project->project_admin_user_id}}">Project Admin</a></h3>
+@endif
+
+<h4><a href="/members/{{ $project->id }}">Members</a></h4>
 
 <div>
 </div>
